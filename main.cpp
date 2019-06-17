dfeb2f5db513c399a99d79bc8b8ffd6a8518d132
Je-vie <896749150@qq.com>
2019-06-17 22:03:07 +0800

    二维数组的使用

--- main.cpp
+++ main.cpp
@@ -4,14 +4,7 @@
 #include "baseCpp/bigIntMul.h"
 #include "myArray/myArraytest.h"
 
-void myArraytest(){
-    myArray<int> arr;
-    arr.reSize<int>(12);
-}
 int main() {
-//    floatInMem();
-//    point_reference_test();
-//    bigint_mult_test();
-    myArraytest();
+
     return 0;
 }
