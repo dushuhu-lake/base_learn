867054b51c7e8843c8cc2d59eaaa5ea0b404940a
Je-vie <896749150@qq.com>
2019-06-19 14:27:48 +0800

    6.19

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "baseCpp/floatInmem.h"
 #include "baseCpp/pointAndreference.h"
-#include "baseCpp/bigIntMul.h"
 #include "myArray/myArraytest.h"
+#include "algorithmCode/jpegZ.h"
+#include "algorithmCode/bigIntMul.h"
+#include "baseCpp/avoid_dump_delete.h"
+#include <string>
+
+
 
 int main() {
+    string s = "liujinfu";
+    string ss = s;
+    s = s+"ninihao ";
+    cout<<ss<<endl;
 
     return 0;
 }
