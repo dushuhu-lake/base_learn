15329fab57305c58c087242b379d56e2563b6b42
Je-vie <896749150@qq.com>
2019-06-19 16:53:22 +0800

    mystring

--- main.cpp
+++ main.cpp
@@ -5,15 +5,11 @@
 #include "algorithmCode/jpegZ.h"
 #include "algorithmCode/bigIntMul.h"
 #include "baseCpp/avoid_dump_delete.h"
+#include "mystring/mystringtest.h"
 #include <string>
 
 
-
 int main() {
-    string s = "liujinfu";
-    string ss = s;
-    s = s+"ninihao ";
-    cout<<ss<<endl;
-
+    mystringtest();
     return 0;
 }
